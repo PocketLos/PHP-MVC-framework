2bd32fb9ada351eb44ba96d7130a04616be006c1
Los <losplanillo@gmail.com>
2020-08-22 23:43:06 +0800

    V1

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    //DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', "__DB__PASS__");
+    define('DB_NAME', '__DB__NAME__');
+
+    // APP ROOT
+    define('APPROOT',dirname(dirname(__FILE__)));
+    // URL ROOT
+    define('URLROOT', 'http://localhost/_YOUR_DB');
+    // Site Name
+    define('SITENAME', '__SITE__NAME__');
